7d644525c857d4349cc6b41efe6b2678ccffdefb
AayuseZz <aayushapradhan766@gmail.com>
2025-01-01 15:15:40 +0900

    Update puti.js

--- scripts/cmds/puti.js
+++ scripts/cmds/puti.js
@@ -24,17 +24,17 @@ module.exports = {
         {
           keywords: ["proxima timro puti", "proxima timro pussy", "proxima puti dekhauna"],
           responseText: "Lau Mero Putie🥵",
-          attachmentPath: path.resolve(__dirname, '545975.small.jpg')
+          attachmentPath: path.resolve(__dirname, '../../../../545975.small.jpg') // Access image from main folder
         },
         {
           keywords: ["fya parana", "fya parana puti", "fyaa parana puti", "fya banauna pussy"],
           responseText: "Huss fyaa banako 🥵🤧",
-          attachmentPath: path.resolve(__dirname, '1000000025.jpg')
+          attachmentPath: path.resolve(__dirname, '../../../../1000000025.jpg') // Access image from main folder
         },
         {
           keywords: ["aajhei fya parana", "ajhei fya banauna"],
           responseText: "Huss🥵 Polyo Pussy💦",
-          attachmentPath: path.resolve(__dirname, 'IMG_20241119_152521.jpg')
+          attachmentPath: path.resolve(__dirname, '../../../../IMG_20241119_152521.jpg') // Access image from main folder
         }
       ];
 
