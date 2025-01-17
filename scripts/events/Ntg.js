909bb56b083549353150db48ffb742a0b562036c
AayuseZz <aayushapradhan766@gmail.com>
2025-01-18 04:08:11 +0900

    Update Ntg.js

--- scripts/events/Ntg.js
+++ scripts/events/Ntg.js
@@ -37,7 +37,7 @@ module.exports = {
           writer.on('finish', async () => {
             try {
               await message.send({
-                body: "Hi, I Am Proxima🤍🌸. Developed by Miss Aayusha Shrestha🤍",
+                body: "Hi, I Am Proxima🤍🌸. Developed by Miss Aayusha Shrestha🤍🕊️",
                 attachment: fs.createReadStream(videoPath),
               });
             } catch (error) {
